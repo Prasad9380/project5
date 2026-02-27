db44e4dd7dbba4a4a61ac02098e563bde250e555
Prasad9380 <prasadmk2004@gmail.com>
2026-02-27 11:38:25 +0530

    Initial commit

---
+++ Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor {
+    public Constructor() {
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String[] args) {
+        Constructor obj = new Constructor();
+    }
+}
